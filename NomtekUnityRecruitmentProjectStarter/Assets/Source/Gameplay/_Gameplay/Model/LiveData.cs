35ffb3790f1fafa6954b57cceb1291910db7cd3a
Eifet <slava.unity.dev@gmail.com>
2022-12-21 16:45:07 +0100

    Created more asmdefs, csproj settings for _ folders

---
+++ NomtekUnityRecruitmentProjectStarter/Assets/Source/Gameplay/_Gameplay/Model/LiveData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nomtek.Source.Gameplay.Model
+{
+    public class LiveData<T>
+    {
+        public event Action<T> OnChanged;
+        
+        T data;
+        public T Data
+        {
+            get => data;
+
+            set
+            {
+                data = value;
+                OnChanged?.Invoke(data);
+            }
+        }
+    }
+}
